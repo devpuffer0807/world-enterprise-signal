5b971db0acc3804a4f3d311ef2482b3fc7e643c6
Puffer <pufferdev0807@gmail.com>
2023-03-23 17:25:43 +0900

    fixed transfer api

--- api/transfer.js
+++ api/transfer.js
@@ -17,19 +17,21 @@ module.exports = async (enterprise, token, from, to, value) => {
 
     const data = {
       enterprise,
+      token,
       from,
       to,
       value,
-      token,
     };
-    console.log("Transfer", enterprise, from, to, value);
+    console.log("Transfer", enterprise, token, from, to, value);
     const queryStr = queryString.stringify(data);
 
-    await axios({
+    const res = await axios({
       method: "POST",
       url: `${EVENT_SIGNAL_URL}/transfer?${queryStr}`,
       headers: { "Content-Type": "application/json; charset=utf-8" },
     });
+
+    console.log("====Transfer Result======", res.data);
 
     return true;
   } catch (e) {
